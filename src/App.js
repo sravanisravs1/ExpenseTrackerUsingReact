c1774214e80f05723b3f414908fc20086570aa2f
sravanisravs1 <kodurusravani813@gmail.com>
2023-02-14 13:59:08 +0530

    created SignUp page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Signup from './components/SignUp/Signup';
+import Header from './components/Header/Header';
+
+var router = createBrowserRouter( [
+  {
+    path: '/',
+    element: <Header/>,
+    children: [
+      { path: '/', element: <Signup/> },
+    ],
+  },
+]);
+function App() {
+  return (
+    <Fragment>
+      <RouterProvider router={router} />
+    </Fragment>
+  );
+}
+
+export default App;
